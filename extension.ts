136e9937937bd6f3716c11b7b3494dfb57ee03e8
Abhijith Chatra <abchatra@microsoft.com>
2021-08-20 11:41:28 -0700

    2D api

--- extension.ts
+++ extension.ts
@@ -2,11 +2,9 @@
  * Build Polygon geometric shapes
  */
 //% weight=94 color=#ffab19 icon="\uf287"
-    namespace polygon {
-    /**
-     * Fill a line of blocks from one position to another.
-     */
-     function drawPolygon(x0: number, y0: number, radius: number, block: number, toWorld: (x: number, y: number) => Position) {
+namespace polygon {
+
+     function draw2DPolygon(x0: number, y0: number, radius: number, block: number, toWorld: (x: number, y: number) => Position) {
         let x = radius;
         let y = 0;
         let err = 0;
@@ -36,7 +34,7 @@
         }
     }
 
-    function fillPolygon(x0: number, y0: number, radius: number, block: number, toWorld: (x: number, y: number) => Position) {
+    function fill2DPolygon(x0: number, y0: number, radius: number, block: number, toWorld: (x: number, y: number) => Position) {
         let x = radius;
         let y = 0;
         let err = 0;
@@ -62,7 +60,7 @@
      * Fill a regular polygon of blocks at a center position.
      * @param radius the radius of the polygon, eg: 5
      */
-    //% blockId=minecraftCircle block="polygon of %block=minecraftBlock|center %center=minecraftCreatePosition|radius %radius|around %orientation|%operator"
+    //% blockId=minecraftPolygon block="polygon of %block=minecraftBlock|center %center=minecraftCreatePosition|radius %radius| sides %sides |around %orientation|%operator"
     //% block.shadow=minecraftBlock
     //% blockExternalInputs=1 weight=95
     //% help=shapes/polygon
@@ -70,13 +68,14 @@
     export function polygon(block: number,
         center: Position,
         radius: number,
+        sides: number,
         orientation: Axis,
         operator: ShapeOperation) {
         if (radius <= 0) return;
 
         if (operator == ShapeOperation.Hollow) {
-            polygon(Block.Air, center, radius, orientation, ShapeOperation.Replace);
-            polygon(block, center, radius, orientation, ShapeOperation.Outline);
+            polygon(Block.Air, center, radius, sides, orientation, ShapeOperation.Replace);
+            polygon(block, center, radius, sides, orientation, ShapeOperation.Outline);
             return;
         }
 
@@ -90,23 +89,23 @@
         if (orientation == Axis.Y) {
             const toWorld = (x: number, z: number) => positions.createWorld(x, yc, z);
             if (operator == ShapeOperation.Outline) {
-                drawPolygon(xc, zc, radius, block, toWorld);
+                draw2DPolygon(xc, zc, radius, sides, block, toWorld);
             } else if (operator == ShapeOperation.Replace) {
-                fillPolygon(xc, zc, radius, block, toWorld);
+                fill2DPolygon(xc, zc, radius, sides, block, toWorld);
             }
         } else if (orientation == Axis.Z) {
             const toWorld = (x: number, y: number) => positions.createWorld(x, y, zc);
             if (operator == ShapeOperation.Outline) {
-                drawPolygon(xc, yc, radius, block, toWorld);
+                draw2DPolygon(xc, yc, radius, sides, block, toWorld);
             } else if (operator == ShapeOperation.Replace) {
-                fillPolygon(xc, yc, radius, block, toWorld);
+                fill2DPolygon(xc, yc, radius, sides, block, toWorld);
             }
         } else { // X axis
             const toWorld = (y: number, z: number) => positions.createWorld(xc, y, z);
             if (operator == ShapeOperation.Outline) {
-                drawPolygon(yc, zc, radius, block, toWorld);
+                draw2DPolygon(yc, zc, radius, sides, block, toWorld);
             } else if (operator == ShapeOperation.Replace) {
-                fillPolygon(yc, zc, radius, block, toWorld);
+                fill2DPolygon(yc, zc, radius, sides, block, toWorld);
             }
         }
     }
